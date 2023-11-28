fbe6e1dea215e3ffa0f006b3b0e3616318d974c1
Abozaid Ibrahim <abozaid.ibrahim11@gmail.com>
2023-11-28 20:15:32 +0200

    Initial Commit

---
+++ MoviesApplication/MoviesApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApplicationApp.swift
+//  MoviesApplication
+//
+//  Created by abuzeid on 28.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
